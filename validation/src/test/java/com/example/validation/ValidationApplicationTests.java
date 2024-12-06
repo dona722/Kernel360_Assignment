ed2506d1cc840a4ad87ddb846609023d9fd97c86
dohun <ysp11487@gmail.com>
2024-12-06 11:02:29 +0900

    validation commit

---
+++ validation/src/test/java/com/example/validation/ValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package src.test.java.com.example.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
